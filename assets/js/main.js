391c53b6334bb3ad59640410c61ef122ec50b97b
Flora Noiret <flo.n@hotmail.fr>
2023-05-04 22:21:48 +0200

    Add Alternate Rule RollDice Player if 1

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,15 +1,24 @@
-function NewGame() {
-    let globalPlayer1 = 0;
-    let globalPlayer2 = 0;
-    let roundPlayer1 = 0;
-    let roundPlayer2 = 0;
-}
+// TO DO Initialization game function
+//function NewGame() { 
+//let globalPlayer1 = 0;
+//let globalPlayer2 = 0;
+// let roundPlayer1 = 0;
+//  let roundPlayer2 = 0;
+//}
 
+// Initialization Game
+let player = [
+    "Player1",
+    "Player2",
+]
 
 let global = 0;
+let globalPlayer1 = 0;
+let globalPlayer2 = 0;
 
 // Set Up Round
 let round = 0;
+let currentplayer = "Player1";
 
 // Roll Dice
 let RollDiceClick = document.getElementById("RollDice").addEventListener("click", (event) => {
@@ -20,25 +29,48 @@ let RollDiceClick = document.getElementById("RollDice").addEventListener("click"
         return Math.floor(Math.random() * (max - min) + min);
     }
 
-    // Conditions on Roll Dice
-    if (RollDice() == 1) {
-        round = 0;
-        console.log("Perdu - Retour à 0");
+    // Player 1 turn
+
+    if (currentplayer == "Player1") {
+
+        // Conditions on Roll Dice
+        if (RollDice() == 1) {
+            round = 0;
+            console.log("Perdu Player1- Retour à 0");
+            currentplayer = "Player2"
+
+        }
+        else {
+            round = (round + RollDice());
+            console.log(round);
+        }
     }
-    else {
-        round = (round + RollDice());
-        console.log(round);
+
+    else { 
+        if (RollDice() == 1) {
+            round = 0;
+            console.log("Perdu Player2- Retour à 0");
+            currentplayer = "Player1"
+
+        }
+        else {
+            round = (round + RollDice());
+            console.log(round);
+        }
+
     }
 });
 
 // Hold Action
 let HoldClick = document.getElementById("Hold").addEventListener("click", (event) => {
-    global = (global + round);
+    globalPlayer1 = (globalPlayer1 + round);
     round = 0;
-    console.log(global);
+    console.log(globalPlayer1);
 
-    if (global >= 100 ) {
-        console.log (global)
-        console.log ("Vous avez gagné la partie");
+    if (global >= 100) {
+        console.log(global)
+        console.log("Vous avez gagné la partie");
     }
 });
+
+
